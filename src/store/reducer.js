0139f15c0d680197a2b52d92ede9431e76ebb122
Aidana Zhumakova <zhumakova99@gmail.com>
2021-09-09 16:23:54 +0600

    Deleted unused state

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,5 @@
 const initialState = {
     password: '',
-    title: '',
     color: ''
 }
 
@@ -10,14 +9,14 @@ const reducer = (state = initialState, action) => {
             return state;
         }
         if (isNaN(state.password)) {
-            return {...state, password: action.payload, color: '', title: ''};
+            return {...state, password: action.payload, color: ''};
         }
-        return {...state, password: state.password + action.payload, title: '', color: ''};
+        return {...state, password: state.password + action.payload, color: ''};
     }
 
     if ((action.type === 'DELETE')) {
         if (isNaN(state.password)) {
-            return {...state, password: '', color: '', title: ''}
+            return {...state, password: '', color: ''}
         }
         const password = state.password;
         const newPassword = password.slice(0, -1);
@@ -25,11 +24,11 @@ const reducer = (state = initialState, action) => {
     }
     if ((action.type === 'CHECK')) {
         if (state.password === '1234') {
-            return {...state, password: 'Access Granted!', title: 'Access Granted!', color: 'green'}
+            return {...state, password: 'Access Granted!', color: 'green'}
         } else if (state.password === '') {
-            return {...state, password: 'Enter PIN Code!', title: 'Enter PIN Code!', color: 'blue'}
+            return {...state, password: 'Enter PIN Code!', color: 'blue'}
         } else {
-            return {...state, password: 'Access Denied!', title: "Access Denied!", color: 'red'}
+            return {...state, password: 'Access Denied!', color: 'red'}
         }
     }
     return state;
